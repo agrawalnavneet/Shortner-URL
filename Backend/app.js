3e4fa9fc3bc24f152cc3b8a0500e4e3afff6fb8f
Navneet Agarwal <agrawalnavneet960@gmail.com>
2025-07-13 13:40:40 +0530

    inital commit

--- Backend/app.js
+++ Backend/app.js
@@ -1,1 +1,30 @@
-//ccr
+import express from "express";
+
+
+dotenv.config("./.env")
+
+const app = express();
+
+app.use(cors({
+    origin: 'http://localhost:5173', // your React app
+    credentials: true // 👈 this allows cookies to be sent
+}));
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(cookieParser())
+
+app.use(attachUser)
+
+app.use("/api/user",user_routes)
+app.use("/api/auth",auth_routes)
+app.use("/api/create",short_url)
+app.get("/:id",redirectFromShortUrl)
+
+app.use(errorHandler)
+
+app.listen(3000,()=>{
+    connectDB()
+    console.log("Server is running on http://localhost:3000");
+})
+
